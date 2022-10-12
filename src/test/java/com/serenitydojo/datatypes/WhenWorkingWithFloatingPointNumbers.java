0dbf37cb161ce3b997cf9c8db07820d6922e460b
k-phi <kraft.philipp@gmail.com>
2022-10-12 20:21:34 +0200

    Do exercise for data types

---
+++ src/test/java/com/serenitydojo/datatypes/WhenWorkingWithFloatingPointNumbers.java
@@ -0,0 +1,39 @@
+package com.serenitydojo.datatypes;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class WhenWorkingWithFloatingPointNumbers {
+
+    //
+    // Exercise 1: Convert 27 degrees Celcius to Farenheit
+    // The equation is: farenheit = (9 / 5) * celcius + 32
+    //
+    @Test
+    public void convertToFarenheit() {
+        final double celcius = 27.0d;
+        double farenheit = 0.0d;
+
+        // TODO: Use a floating point calculation to calculate the farenheit equivalent of the celcius value.
+        farenheit =  (9.0 / 5.0) * celcius + 32.0;
+        assertThat(farenheit, equalTo(80.6));
+    }
+
+    //
+    // Exercise 2: Convert kilograms to pounds
+    // There are 2.20462 pounds in a kilogram
+    //
+    @Test
+    public void convertMetersToFeet() {
+        final int weightInKilograms = 50;
+        double weightInPounds = 0;
+
+        // TODO: Use a floating point calculation to calculate the correct weight in pounds
+        final double poundsPerKilogram = 2.20462;
+        weightInPounds = poundsPerKilogram * weightInKilograms;
+        assertThat(weightInPounds, equalTo(110.231));
+
+    }
+}
